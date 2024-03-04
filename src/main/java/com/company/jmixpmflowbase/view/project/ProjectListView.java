85732db65a36bc312ab3b32d28b81741d0d932cd
vashenkoda <d.vaschenko@haulmont.net>
2024-03-04 16:55:23 +0300

    Test Jmix changes

--- src/main/java/com/company/jmixpmflowbase/view/project/ProjectListView.java
+++ src/main/java/com/company/jmixpmflowbase/view/project/ProjectListView.java
@@ -4,8 +4,15 @@ import com.company.jmixpmflowbase.entity.Project;
 
 import com.company.jmixpmflowbase.view.main.MainView;
 
+import com.company.jmixpmflowbase.view.task.TaskListView;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "projects", layout = MainView.class)
 @ViewController("Project.list")
@@ -13,4 +20,28 @@ import io.jmix.flowui.view.*;
 @LookupComponent("projectsDataGrid")
 @DialogMode(width = "64em")
 public class ProjectListView extends StandardListView<Project> {
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private DialogWindows dialogWindows;
+
+    @Supply(to = "projectsDataGrid.showTasks", subject = "renderer")
+    private Renderer<Project> projectsDataGridShowTasksRenderer() {
+        return new ComponentRenderer<> (project -> {
+            JmixButton linkButton = uiComponents.create(JmixButton.class);
+            linkButton.setText("Show Tasks");
+            linkButton.addClickListener(clickEvent -> {
+                openTasksDialog(project);
+            });
+            return linkButton;
+        });
+    }
+
+    private void openTasksDialog(Project project) {
+        DialogWindow<TaskListView> tasksDialog = dialogWindows.view(this, TaskListView.class)
+                .build();
+        tasksDialog.getView().withProject(project);
+        tasksDialog.open();
+
+    }
 }
--- src/main/java/com/company/jmixpmflowbase/view/task/TaskListView.java
+++ src/main/java/com/company/jmixpmflowbase/view/task/TaskListView.java
@@ -1,10 +1,12 @@
 package com.company.jmixpmflowbase.view.task;
 
+import com.company.jmixpmflowbase.entity.Project;
 import com.company.jmixpmflowbase.entity.Task;
 
 import com.company.jmixpmflowbase.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 
 @Route(value = "tasks", layout = MainView.class)
@@ -13,4 +15,17 @@ import io.jmix.flowui.view.*;
 @LookupComponent("tasksDataGrid")
 @DialogMode(width = "64em")
 public class TaskListView extends StandardListView<Task> {
+    @ViewComponent
+    private CollectionLoader<Task> tasksDl;
+
+    public void withProject(Project project){
+        if(project != null){
+            tasksDl.setParameter("project", project);
+        }
+        else {
+            tasksDl.removeParameter("project");
+        }
+
+        tasksDl.load();
+    }
 }
